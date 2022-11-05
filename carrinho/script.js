7cc20f279b5cb265da7f6239105108ac878f68d4
Gabriel Grubba <Grubba27@hotmail.com>
2022-11-05 12:18:09 -0300

    adjusted

--- carrinho/script.js
+++ carrinho/script.js
@@ -13,14 +13,6 @@ function intoArray(obj) {
   return Object.keys(obj).map((id) => obj[id]);
 }
 export async function getProducts(phpFile = "./server/get-carrinho.php") {
-  let cart = localStorage.getItem("carrinho") || {};
-  if (typeof cart == "string") cart = JSON.parse(cart);
-  if (isEmpty(cart)) {
-    return {
-      products: [],
-      cart,
-    };
-  }
   const { data, error } = await query(phpFile, {
     method: "POST",
     extraHeaders: {
@@ -28,13 +20,11 @@ export async function getProducts(phpFile = "./server/get-carrinho.php") {
     },
     body: JSON.stringify(cart),
   });
-  console.log(data);
   return {
     products: intoArray(data),
-    cart,
   };
 }
-function renderProducts(products, cart) {
+function renderProducts(products) {
   if (products.length === 0) {
     render("cart", html`<h1>Carrinho vazio</h1>`);
     return;
@@ -47,8 +37,8 @@ function renderProducts(products, cart) {
         <h2>${product.nome}</h2>
         <p>${product.descricao}</p>
         <p>R$ ${product.valor}</p>
-        <p>Quantidade: ${cart[product.id].qtd}</p>
-        <p>Subtotal: R$ ${cart[product.id].qtd * product.valor}</p>
+        <p>Quantidade: ${product.qtd}</p>
+        <p>Subtotal: R$ ${product.qtd * product.valor}</p>
         <button onclick="addToCart(${product.id})">Adicionar mais um</button>
         <button onclick="removeFromCart(${product.id})">Remover</button>
       </div>`;
@@ -74,27 +64,33 @@ function renderTotal(total) {
 }
 async function onMount() {
   // verify if is logged
-  const { products, cart } = await getProducts("./server/get-carrinho.php");
+  const { products } = await getProducts("./server/get-carrinho.php");
   const total = products.reduce(
-    (acc, product) => acc + cart[product.id].qtd * product.valor,
+    (acc, product) => acc + product.qtd * product.valor,
     0
   );
-  renderProducts(products, cart);
+  renderProducts(products);
   renderTotal(total);
 }
 
 window.addToCart = async function (id) {
-  cart.addToCart(id); // local
+  // cart.addToCart(id); // local
+  const user = localStorage.getItem("user");
+  const userId = JSON.parse(user).id;
   const { data, error } = await query("./server/add-carrinho.php", {
-    body: JSON.stringify({id}),
+    body: JSON.stringify({ id, userId }),
     method: "POST",
   });
-  console.log(data);
   onMount();
 };
 
-window.removeFromCart = function (id) {
-  cart.removeFromCart(id);
+window.removeFromCart = async function (id) {
+  const user = localStorage.getItem("user");
+  const userId = JSON.parse(user).id;
+  const { data, error } = await query("./server/remove-carrinho.php", {
+    body: JSON.stringify({ id, userId }),
+    method: "POST",
+  });
   onMount();
 };
 
