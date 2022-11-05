325587437770fb6eb636d74e971c9fe91f21c356
Gabriel Grubba <Grubba27@hotmail.com>
2022-11-05 11:39:43 -0300

    added more

--- carrinho/server/get-carrinho.php
+++ carrinho/server/get-carrinho.php
@@ -4,3 +4,8 @@ $id = $dt['userId'];
 // select from carrinho where userId {3: {qtd: 3}, 5: {qtd:1}}
 // select na tabela de produtos where idprodutoCarrinho e traga os produtos 
 // pega o retorno da tabela e passa de volta pro front
+
+echo json_encode([
+  '1' => ['qtd' => 16],
+  '2' => ['qtd' => 4],
+]);
